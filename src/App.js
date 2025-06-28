4abbce5375f5abf7c1c96a2b55338e01afc66ee8
vardhan-venkata <vardhanmerapureddy@gmail.com>
2025-06-28 19:36:22 +0530

    IMplmeneted the case insensitive search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import logo from "./logo.svg";
+import { useState } from "react";
+
+const dictionaryItems = [
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." },
+];
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const [definition, setDefinition] = useState("");
+  const handleSearchClick = () => {
+    let searchedItem =
+      dictionaryItems &&
+      dictionaryItems.length > 0 &&
+      dictionaryItems.find(
+        (ele) => ele.word?.toLocaleLowerCase() === searchValue?.toLowerCase()
+      );
+    if (searchedItem) {
+      setDefinition(searchedItem.meaning);
+    } else {
+      setDefinition("Word not found in the dictionary.");
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dictionary App.</h1>
+      <div>
+        <input
+          placeholder="Search for a word..."
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <button onClick={() => handleSearchClick()}>Search</button>
+      </div>
+      <h3>Definition:</h3> <p>{definition}</p>
     </div>
   );
 }
